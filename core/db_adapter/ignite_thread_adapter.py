ab9e2b9afb2574097fa2ef173c7917efad736e83
AivanF <IABarashev@sberbank.ru>
2021-03-22 13:01:21 +0300

    Moved Ignite Thread adapter in other file

--- core/db_adapter/ignite_thread_adapter.py
+++ core/db_adapter/ignite_thread_adapter.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import random
+import threading
 
 import pyignite
 from pyignite.exceptions import ReconnectError, SocketError
@@ -11,7 +12,7 @@ from core.logging.logger_utils import log
 from core.monitoring.monitoring import monitoring
 
 
-class IgniteAdapter(DBAdapter):
+class IgniteThreadAdapter(DBAdapter):
 
     def __init__(self, config):
         self._init_params = config.get("init_params", {})
@@ -19,8 +20,9 @@ class IgniteAdapter(DBAdapter):
         if config.get("randomize_url"):
             random.shuffle(self._url)
         self._cache_name = config["cache_name"]
-        self._cache = None
-        super(IgniteAdapter, self).__init__(config)
+        self._clients = {}
+        self._caches = {}
+        super(IgniteThreadAdapter, self).__init__(config)
 
     def _open(self, filename, *args, **kwargs):
         pass
@@ -35,10 +37,15 @@ class IgniteAdapter(DBAdapter):
         raise error.NotSupportedOperation
 
     def connect(self):
+        self._get_cache()
+
+    def _connect_thread(self, thread_id):
         try:
-            self._client = pyignite.Client(**self._init_params)
-            self._client.connect(self._url)
-            self._cache = self._client.get_or_create_cache(self._cache_name)
+            client = pyignite.Client(**self._init_params)
+            client.connect(self._url)
+            cache = client.get_or_create_cache(self._cache_name)
+            self._clients[thread_id] = client
+            self._caches[thread_id] = cache
             logger_args = {
                 log_const.KEY_NAME: log_const.IGNITE_VALUE,
                 "pyignite_args": str(self._init_params),
@@ -46,30 +53,38 @@ class IgniteAdapter(DBAdapter):
             }
             log("IgniteAdapter to servers %(pyignite_addresses)s created", params=logger_args, level="WARNING")
         except Exception:
-            log("IgniteAdapter connect error",
-                          params={log_const.KEY_NAME: log_const.HANDLED_EXCEPTION_VALUE},
-                          level="ERROR",
-                          exc_info=True)
+            log(
+                "IgniteAdapter connect error",
+                params={log_const.KEY_NAME: log_const.HANDLED_EXCEPTION_VALUE},
+                level="ERROR",
+                exc_info=True
+            )
             monitoring.got_counter("ignite_connection_exception")
             raise
 
+    def _get_cache(self):
+        thread_id = threading.get_ident()
+        if thread_id not in self._caches:
+            self._connect_thread(thread_id)
+        return self._caches[thread_id]
+
     def _save(self, id, data):
-        return self.cache.put(id, data)
+        return self._get_cache().put(id, data)
 
     def _replace_if_equals(self, id, sample, data):
-        return self._cache.replace_if_equals(id, sample, data)
+        return self._get_cache().replace_if_equals(id, sample, data)
 
     def _get(self, id):
-        data = self.cache.get(id)
+        data = self._get_cache().get(id)
         return data
 
     @property
     def cache(self):
-        if self._cache is None:
+        if self._get_cache() is None:
             log('Attempt to recreate ignite instance', level="WARNING")
             self.connect()
             monitoring.got_counter("ignite_reconnection")
-        return self._cache
+        return self._get_cache()
 
     @property
     def _handled_exception(self):

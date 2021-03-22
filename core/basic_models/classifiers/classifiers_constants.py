063b9769c8b6bbd53707969b66443f363d311a6d
Ksenia Voronaya <ksenia.voronaya@yandex.ru>
2021-03-22 14:34:38 +0300

    Add ClassiferFiller in hello scenario

--- core/basic_models/classifiers/classifiers_constants.py
+++ core/basic_models/classifiers/classifiers_constants.py
@@ -6,7 +6,7 @@ ANSWER_KEY = "answer"
 OTHER_KEY = "other"
 
 # Дополнительные константы для внешних "external" классификаторов
-EXTERNAL_CLASSIFIER_BLOCKING_TIMEOUT = 0.2  # Время (в секундах) за которое классификатор должен успеть ответить
+EXTERNAL_CLASSIFIER_BLOCKING_TIMEOUT = 0.4  # Время (в секундах) за которое классификатор должен успеть ответить
 
 # Константы классификатора Intent Recognizer
 INTENT_RECOGNIZER_ANSWER_DISTANCE_KEY = "answer_distance"

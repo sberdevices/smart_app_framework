f5b6feb2a1d5cefa0cad758ab12ff3036bed9d8f
Ksenia Voronaya <ksenia.voronaya@yandex.ru>
2021-04-06 19:06:58 +0300

    Load predefined_fields_storage once, when local testing starts

--- smart_kit/testing/local.py
+++ smart_kit/testing/local.py
@@ -40,6 +40,9 @@ class CLInterface(cmd.Cmd):
         self.__user_cls = user_cls
         self.__parametrizer_cls = parametrizer_cls
 
+        with open(str(os.path.join(self.references_path, "./predefined_fields_storage.json")), "r") as f:
+            self.storaged_predefined_fields = json.load(f)
+
     def after_process_message(self, message) -> typing.Optional[str]:
         callback = getattr(self, f"on_{message.name.lower()}", lambda *args, **kwargs: None)
         return callback(message)
@@ -107,6 +110,7 @@ class CLInterface(cmd.Cmd):
                 self.settings,
                 self.__user_cls,
                 self.__parametrizer_cls,
+                self.storaged_predefined_fields
             )
         except FileNotFoundError:
             print("Нужно указать относительный путь до json файла.")

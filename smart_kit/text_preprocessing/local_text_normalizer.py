33a733f99a74a5f0ced899f8c427bd64ecf8a7c5
Danil Davydyan <DADavydyan@sberbank.ru>
2021-07-31 16:39:36 +0300

    переименовал RNNMorphWrapper в Pymorpy2MorphWrapper

--- smart_kit/text_preprocessing/local_text_normalizer.py
+++ smart_kit/text_preprocessing/local_text_normalizer.py
@@ -8,7 +8,7 @@ from core.utils.loader import ordered_json
 
 from smart_kit.text_preprocessing.base_text_normalizer import BaseTextNormalizer
 from smart_kit.text_preprocessing.nltk_tokenizer_binding import NLTKWordTokenizer
-from smart_kit.text_preprocessing.rnnmorph_wrapper import Pymorpy2MorphWrapper
+from smart_kit.text_preprocessing.pymorphy2_morph_wrapper import Pymorpy2MorphWrapper
 from smart_kit.text_preprocessing.text2num import Text2Num, NumbersUnionAfterSTT
 from smart_kit.text_preprocessing.utils import replace_yo_to_e, unmerge_numbers_and_letters, merge_numbers, \
     replace_currencies_symbols, AdditionalMathSplitter, MergeCardNumbersVoice, MergeCardNumbers, \

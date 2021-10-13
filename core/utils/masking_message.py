9b8bc5392d226a6ddbf74f09bd36bf5f717f3b25
Artem Batalov <aebatalov@sberbank.ru>
2021-10-13 12:38:44 +0300

    DPNLPF-1297: Fixes

--- core/utils/masking_message.py
+++ core/utils/masking_message.py
@@ -1,4 +1,4 @@
-from collections import Iterable, Mapping
+from typing import Callable, Optional, Iterable, Mapping, Union, Pattern, Match, MutableMapping
 import re
 import traceback
 
@@ -6,41 +6,46 @@ MASK = "***"
 DEFAULT_MASKING_FIELDS = ["token", "access_token", "refresh_token", "epkId", "profileId"]
 CARD_MASKING_FIELDS = ["message"]
 
+card_regular = re.compile("(?:^|\s|\'|\")(?:(\d{18})|(\d{16})|(?:\d{4} ){3}(\d{4})(\s?\d{2})?)(?:$|\W)")
 
-def card_masking(record):
-    card_regular = re.compile("(?:^|\s|\'|\")(?:(\d{18})|(\d{16})|(?:\d{4} ){3}(\d{4})(\s?\d{2})?)(?:$|\W)")
+
+def card_sub_func(x: Match[str]) -> str:
     d_regular = re.compile("\d")
 
-    def sub_func(x):
-        g0 = x.group(0)
-        is_last_not_digit = int(g0 and not g0[-1].isdigit())
-        last_char = g0[-1]
+    g0 = x.group(0)
+    is_last_not_digit = int(g0 and not g0[-1].isdigit())
+    last_char = g0[-1]
 
-        mask = d_regular.sub("*", x.group(0))[:-(4 + is_last_not_digit)]
-        digs = ((x.group(1) or '') + (x.group(2) or '') + (x.group(3) or '') + (x.group(4) or '')).replace(' ', '')[-4:]
-        return mask + digs + (last_char * is_last_not_digit)
+    mask = d_regular.sub("*", x.group(0))[:-(4 + is_last_not_digit)]
+    digs = ((x.group(1) or '') + (x.group(2) or '') + (x.group(3) or '') + (x.group(4) or '')).replace(' ', '')[-4:]
+    return mask + digs + (last_char * is_last_not_digit)
 
+
+def regex_masking(record: Union[Mapping, str, Iterable], regex: Pattern[str], func: Callable[[Match[str]], str]) \
+        -> Union[Mapping, str, Iterable]:
     if isinstance(record, Mapping):
-        return {k: card_masking(v) for k, v in record.items()}
+        return {k: regex_masking(v, regex, func) for k, v in record.items()}
     elif isinstance(record, str):
-        return card_regular.sub(sub_func, record)
+        return regex.sub(func, record)
     elif isinstance(record, Iterable):
-        return [card_masking(i) for i in record]
+        return [regex_masking(i, regex, func) for i in record]
     else:
         return record
 
 
-def masking(data, masking_fields=None):
+def masking(data: MutableMapping, masking_fields: Optional[list] = None):
     if masking_fields is None:
         masking_fields = DEFAULT_MASKING_FIELDS
-    if hasattr(data, 'items'):
+    if isinstance(data, MutableMapping):
         for key in data:
             if key in masking_fields:
                 data[key] = MASK
             elif key in CARD_MASKING_FIELDS:
-                data[key] = card_masking(data[key])
-            elif isinstance(data[key], dict):
+                data[key] = regex_masking(data[key], card_regular, card_sub_func)
+            elif isinstance(data[key], str):
+                pass
+            elif isinstance(data[key], MutableMapping):
                 masking(data[key], masking_fields)
-            elif isinstance(data[key], list):
+            elif isinstance(data[key], Iterable):
                 for item in data[key]:
                     masking(item, masking_fields)

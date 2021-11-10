90b91b08767d5a7b50070d55a5baf1b3d75c4687
Andrey Pushnin <pushnin.a.an@sberbank.ru>
2021-11-10 14:42:07 +0300

    UpdatableFileRepository to template_config.yml

--- smart_kit/configs/settings.py
+++ smart_kit/configs/settings.py
@@ -19,7 +19,7 @@ class Settings(BaseConfig):
         self.app_name = kwargs.get("app_name")
         self.adapters = {Settings.CephAdapterKey: CephAdapter, self.OSAdapterKey: OSAdapter}
         self.repositories = [
-            FileRepository(
+            UpdatableFileRepository(
                 self.subfolder_path("template_config.yml"), loader=yaml.safe_load, key="template_settings"
             ),
             FileRepository(self.subfolder_secret_path("kafka_config.yml"), loader=yaml.safe_load, key="kafka"),

1c76120c8e5c0c08b860e01964e50ba35ec71812
intsynko <intsyn@mail.ru>
2021-12-21 10:19:16 +0300

    added disabling specific metrix from monitoring by matching names

--- core/monitoring/monitoring.py
+++ core/monitoring/monitoring.py
@@ -9,12 +9,15 @@ class Monitoring:
 
     def __init__(self):
         self._enabled = self.DEFAULT_ENABLED
-        self._monitoring_items = {self.COUNTER: {},
-                                  self.HISTOGRAM: {}
-                                  }
+        self._monitoring_items = {
+            self.COUNTER: {},
+            self.HISTOGRAM: {}
+        }
+        self.disabled_metrics = []
 
-    def check_enabled(self):
-        return self._enabled
+    def check_enabled(self, name: str):
+        metric_disabled = next((True for m in self.disabled_metrics if name.startswith(m)), False)
+        return self._enabled and not metric_disabled
 
     def turn_on(self):
         self._enabled = True
@@ -23,25 +26,40 @@ class Monitoring:
         self._enabled = False
 
     def got_counter(self, name):
-        if self.check_enabled():
+        if self.check_enabled(name):
             counter = self._monitoring_items[self.COUNTER]
             if not counter.get(name):
                 counter[name] = Counter(name, name)
             counter[name].inc()
 
     def got_histogram(self, name):
-        if self.check_enabled():
+        def decor(func):
+            def wrap(*args, **kwargs):
+                decor_ = self._got_histogram(name)
+                if decor_:
+                    return decor_(func)(*args, **kwargs)
+                else:
+                    return func(*args, **kwargs)
+            return wrap
+        return decor
+
+    def _got_histogram(self, name):
+        if self.check_enabled(name):
             histogram = self._monitoring_items[self.HISTOGRAM]
             if not histogram.get(name):
                 histogram[name] = Histogram(name, name)
             return histogram[name].time()
 
     def got_histogram_observe(self, name, value):
-        if self.check_enabled():
+        if self.check_enabled(name):
             histogram = self._monitoring_items[self.HISTOGRAM]
             if not histogram.get(name):
                 histogram[name] = Histogram(name, name)
             return histogram[name].observe(value)
+
+    def apply_config(self, config):
+        self._enabled = config.get("enabled", self.DEFAULT_ENABLED)
+        self.disabled_metrics = config.get("disabled_metrics", [])
 
 
 monitoring = Monitoring()
--- tests/core_tests/monitoring_test/test_monitoring.py
+++ tests/core_tests/monitoring_test/test_monitoring.py
@@ -16,41 +16,49 @@ class MonitoringTest1(unittest.TestCase):
 
     def test_got_message_disabled(self):
         self.monitoring.turn_off()
-        self.monitoring._monitoring_items[ self.monitoring.COUNTER ] = {}
         event_name = "test_counter"
-        counter_item = self.monitoring._monitoring_items[ self.monitoring.COUNTER ]
+        counter_item = self.monitoring._monitoring_items[self.monitoring.COUNTER]
         self.assertTrue(counter_item == dict())
-        counter = self.monitoring.got_histogram(event_name)
+        counter = self.monitoring.got_counter(event_name)
         self.assertTrue(event_name not in counter_item)
         self.assertIsNone(counter)
 
     def test_got_message_enabled(self):
         self.monitoring.turn_on()
-        self.monitoring._monitoring_items[ self.monitoring.COUNTER ] = {}
         event_name = "test_counter"
-        counter_item = self.monitoring._monitoring_items[ self.monitoring.COUNTER ]
+        counter_item = self.monitoring._monitoring_items[self.monitoring.COUNTER]
         self.assertTrue(counter_item == dict())
         self.monitoring.got_counter(event_name)
         self.assertTrue(event_name in counter_item)
-        self.assertEqual(type(counter_item[ event_name ]), type(Counter('counter_name', 'counter_name')))
+        self.assertEqual(type(counter_item[event_name]), type(Counter('counter_name', 'counter_name')))
 
     def test_got_histogram_disabled(self):
         self.monitoring.turn_off()
-        self.monitoring._monitoring_items[ self.monitoring.HISTOGRAM ] = {}
         event_name = "test_histogram"
-        histogram_item = self.monitoring._monitoring_items[ self.monitoring.HISTOGRAM ]
+        histogram_item = self.monitoring._monitoring_items[self.monitoring.HISTOGRAM]
         self.assertTrue(histogram_item == dict())
-        histogram = self.monitoring.got_histogram(event_name)
+        histogram = self.monitoring._got_histogram(event_name)
         self.assertTrue(event_name not in histogram_item)
         self.assertIsNone(histogram)
 
     def test_got_histogram_enabled(self):
         self.monitoring.turn_on()
-        self.monitoring._monitoring_items[self.monitoring.HISTOGRAM] = {}
         event_name = "test_histogram"
         histogram_item = self.monitoring._monitoring_items[self.monitoring.HISTOGRAM]
         self.assertTrue(histogram_item == dict())
-        self.monitoring.got_histogram(event_name)
+        self.monitoring._got_histogram(event_name)
         histogram_item = self.monitoring._monitoring_items[self.monitoring.HISTOGRAM]
         self.assertTrue(event_name in histogram_item)
         self.assertEqual(type(histogram_item[event_name]), type(Histogram('histogram_name', 'histogram_name')))
+
+    def test_got_histogram_disabled_by_name(self):
+        self.monitoring.turn_on()
+        self.monitoring.disabled_metrics.append('test_')
+        for event_name, disabled in [("test_one", True), ("test_two", True), ("not_a_test", False)]:
+            counter_item = self.monitoring._monitoring_items[self.monitoring.COUNTER]
+            self.assertTrue(counter_item == dict())
+            self.monitoring.got_counter(event_name)
+            if disabled:
+                self.assertTrue(event_name not in counter_item)
+            else:
+                self.assertTrue(event_name in counter_item)

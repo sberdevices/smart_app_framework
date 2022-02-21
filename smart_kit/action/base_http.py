914d7da1505f07215fee2e60b0e83c863ce221ad
intsynko <intsyn@mail.ru>
2022-02-21 13:40:52 +0300

    DPNLPF-1536 added additional params for logging

--- smart_kit/action/base_http.py
+++ smart_kit/action/base_http.py
@@ -64,8 +64,8 @@ class BaseHttpRequestAction(NodeAction):
         except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError):
             self.error = self.CONNECTION
 
-    def _get_requst_params(self, user: BaseUser, text_preprocessing_result: BaseTextPreprocessingResult,
-            params: Optional[Dict[str, Union[str, float, int]]] = None):
+    def _get_request_params(self, user: BaseUser, text_preprocessing_result: BaseTextPreprocessingResult,
+                            params: Optional[Dict[str, Union[str, float, int]]] = None):
         collected = user.parametrizer.collect(text_preprocessing_result)
         params.update(collected)
 
@@ -78,13 +78,15 @@ class BaseHttpRequestAction(NodeAction):
             request_parameters["headers"] = self._check_headers_validity(req_headers)
         return request_parameters
 
-    def _log_request(self, user, request_parameters):
+    def _log_request(self, user, request_parameters, additional_params=None):
         log(f"{self.__class__.__name__}.run sent https request ", user=user, params={
             **request_parameters,
-            log_const.KEY_NAME: "sent_http_request"
+            log_const.KEY_NAME: "sent_http_request",
+            **additional_params,
         })
 
-    def _log_response(self, user, response, data):
+    def _log_response(self, user, response, data, additional_params=None):
+        additional_params = additional_params or {}
         log(f"{self.__class__.__name__}.run get https response ", user=user, params={
             'headers': dict(response.headers),
             'time': response.elapsed.microseconds,
@@ -92,11 +94,12 @@ class BaseHttpRequestAction(NodeAction):
             'status': response.status_code,
             'data': data,
             log_const.KEY_NAME: "got_http_response",
+            **additional_params,
         })
 
     def run(self, user: BaseUser, text_preprocessing_result: BaseTextPreprocessingResult,
             params: Optional[Dict[str, Union[str, float, int]]] = None) -> Optional[List[Command]]:
         params = params or {}
-        request_parameters = self._get_requst_params(user, text_preprocessing_result, params)
+        request_parameters = self._get_request_params(user, text_preprocessing_result, params)
         self._log_request(user, request_parameters)
         return self._make_response(request_parameters, user)

eb12c115f189ed904a6340a0e3431e8aa929b4d8
Artem Batalov <aebatalov@sberbank.ru>
2022-02-21 10:28:52 +0300

    HTTP request returned entire Response

---
+++ smart_kit/action/base_http_headers.py
@@ -0,0 +1,31 @@
+import json
+from typing import Optional, Dict, Union, List
+
+import requests
+from requests import Response
+
+from core.basic_models.actions.command import Command
+from core.model.base_user import BaseUser
+from core.text_preprocessing.base import BaseTextPreprocessingResult
+from smart_kit.action.base_http import BaseHttpRequestAction
+
+
+class BaseHttpRequestActionWithHeaders(BaseHttpRequestAction):
+    def _make_response(self, request_parameters, user):
+        try:
+            with requests.request(**request_parameters) as response:
+                response.raise_for_status()
+                try:
+                    data = response
+                except json.decoder.JSONDecodeError:
+                    data = None
+                self._log_response(user, response, data)
+                return data
+        except requests.exceptions.Timeout:
+            self.error = self.TIMEOUT
+        except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError):
+            self.error = self.CONNECTION
+
+    def run(self, user: BaseUser, text_preprocessing_result: BaseTextPreprocessingResult,
+            params: Optional[Dict[str, Union[str, float, int]]] = None) -> Optional[Response]:
+        return super().run(user, text_preprocessing_result, params)

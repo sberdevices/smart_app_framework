e6d3d5fca0a8e119a8355876c503b3dde73625a0
Ildus <sharifullin.ildus@list.ru>
2022-03-15 17:32:09 +0300

    DPNLPF-1446: add finished field to log params

--- core/logging/logger_utils.py
+++ core/logging/logger_utils.py
@@ -74,7 +74,10 @@ def log(message, user=None, params=None, level="INFO", exc_info=None, log_store_
         module_name = previous_frame.f_globals["__name__"]
         logger = logging.getLogger(module_name)
         instance = previous_frame.f_locals.get('self', None)
-        params['finished'] = user.message.payload.get("finished", None) if user else None
+        if user:
+            params['finished'] = user.message.payload.get("finished", None)
+        else:
+            params['finished'] = None
 
         from smart_kit.configs import get_app_config
         try:

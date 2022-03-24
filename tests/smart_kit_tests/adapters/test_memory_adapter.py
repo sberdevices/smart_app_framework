14930ddae5ec709ef648f9e5e18c95461f9d55d7
Makar Shevchenko <makarsevchenko@gmail.com>
2022-03-24 21:30:08 +0300

    DPNLPF-1300: make children of AsyncDBAdapter async

--- tests/smart_kit_tests/adapters/test_memory_adapter.py
+++ tests/smart_kit_tests/adapters/test_memory_adapter.py
@@ -4,50 +4,50 @@ import unittest
 from core.db_adapter import memory_adapter
 
 
-class AdapterTest1(unittest.TestCase):
+class AdapterTest1(unittest.IsolatedAsyncioTestCase):
 
-    def test_memory_adapter_init(self):
+    async def test_memory_adapter_init(self):
         obj1 = memory_adapter.MemoryAdapter()
         obj2 = memory_adapter.MemoryAdapter({'try_count': 3})
         self.assertTrue(hasattr(obj1, 'open'))
         self.assertTrue(hasattr(obj2, 'open'))
-        self.assertTrue(obj1.memory_storage == {})
-        self.assertTrue(obj2.memory_storage == {})
-        self.assertTrue(obj1.try_count == 5)  # взято из исходников
-        self.assertTrue(obj2.try_count == 3)
+        self.assertEqual(obj1.memory_storage, {})
+        self.assertEqual(obj2.memory_storage, {})
+        self.assertEqual(obj1.try_count, 5)  # взято из исходников
+        self.assertEqual(obj2.try_count, 3)
 
-    def test_memory_adapter_connect(self):
+    async def test_memory_adapter_connect(self):
         obj = memory_adapter.MemoryAdapter()
         self.assertTrue(hasattr(obj, 'connect'))
 
-    def test_memory_adapter_open(self):
+    async def test_memory_adapter_open(self):
         obj = memory_adapter.MemoryAdapter()
         self.assertTrue(hasattr(obj, '_open'))
         with self.assertRaises(TypeError):
-            obj._open()
+            await obj._open()
 
-    def test_memory_adapter_save(self):
+    async def test_memory_adapter_save(self):
         obj1 = memory_adapter.MemoryAdapter()
         obj2 = memory_adapter.MemoryAdapter()
-        obj1._save(10, {'any_data'})
-        obj1._save(11, {'any_data'})
-        obj2._save(10, {'any_data'})
-        self.assertTrue(obj1.memory_storage == {10: {'any_data'}, 11: {'any_data'}})
-        self.assertTrue(obj2.memory_storage == {10: {'any_data'}})
+        await obj1._save(10, {'any_data'})
+        await obj1._save(11, {'any_data'})
+        await obj2._save(10, {'any_data'})
+        self.assertEqual(obj1.memory_storage, {10: {'any_data'}, 11: {'any_data'}})
+        self.assertEqual(obj2.memory_storage, {10: {'any_data'}})
         # метод переписывает значения
-        obj2._save(10, 'any_data')
-        self.assertTrue(obj2.memory_storage == {10: 'any_data'})
+        await obj2._save(10, 'any_data')
+        self.assertEqual(obj2.memory_storage, {10: 'any_data'})
 
-    def test_memory_adapter_get(self):
+    async def test_memory_adapter_get(self):
         obj1 = memory_adapter.MemoryAdapter()
-        obj1._save(10, {'any_data'})
-        obj1._save(11, 'any_data')
-        self.assertTrue(obj1._get(10) == {'any_data'})
-        self.assertTrue(obj1._get(11) == 'any_data')
-        self.assertIsNone(obj1._get(12))
+        await obj1._save(10, {'any_data'})
+        await obj1._save(11, 'any_data')
+        self.assertEqual(await obj1._get(10), {'any_data'})
+        self.assertEqual(await obj1._get(11), 'any_data')
+        self.assertIsNone(await obj1._get(12))
 
-    def test_memory_adapter_list_dir(self):
+    async def test_memory_adapter_list_dir(self):
         obj = memory_adapter.MemoryAdapter()
         self.assertTrue(hasattr(obj, '_list_dir'))
         with self.assertRaises(TypeError):
-            obj._open()
+            await obj._open()

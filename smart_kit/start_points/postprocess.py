779af2db87414de8f0b87dfdddb8e53d79e95897
intsynko <intsyn@mail.ru>
2022-03-24 14:33:17 +0300

    refactoring postprocessor_compose

--- smart_kit/start_points/postprocess.py
+++ smart_kit/start_points/postprocess.py
@@ -7,11 +7,15 @@ class Postprocess:
         pass
 
 
-def postprocessor_compose(*agrs: List[Type[Postprocess]]):
-    class PostprocessCompose(Postprocess):
-        postprocessors: List[Postprocess] = [processor_cls() for processor_cls in agrs]
+class PostprocessCompose(Postprocess):
+    postprocessors: List[Postprocess] = []
 
-        def postprocess(self, user, message, *args, **kwargs):
-            for processor in self.postprocessors:
-                processor.postprocess(user, message, *args, **kwargs)
-    return PostprocessCompose
+    def postprocess(self, user, message, *args, **kwargs):
+        for processor in self.postprocessors:
+            processor.postprocess(user, message, *args, **kwargs)
+
+
+def postprocessor_compose(*agrs: List[Type[Postprocess]]):
+    class Compose(PostprocessCompose):
+        postprocessors = [processor_cls() for processor_cls in agrs]
+    return Compose

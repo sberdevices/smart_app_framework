2a34f516929c8cd97b1f6c0f6dd5a12644b4ca63
Dan1lD <davidian654@gmail.com>
2022-04-08 11:07:49 +0300

    add examples for smartpay

--- core/basic_models/actions/smartpay.py
+++ core/basic_models/actions/smartpay.py
@@ -35,6 +35,125 @@ class SmartPayAction(HTTPRequestAction):
 
 
 class SmartPayCreateAction(SmartPayAction):
+
+    """
+    Example::
+        {
+            "type": "smartpay_create",
+            "behavior": "some_behavior", // обязательный параметр
+            "smartpay_params": { // обязательноый параметр
+                "ptype": 1,
+                "invoice": {
+                    "purchaser": {
+                        "email": "test@test.ru",
+                        "phone": "71111111111",
+                        "contact": "phone"
+                    },
+                    "delivery_info": {
+                        "address": {
+                            "country": "RU",
+                            "city": "Москва",
+                            "address": "Кутузовский проспект, 32"
+                        },
+                        "delivery_type": "courier",
+                        "description": "Спросить '\''Где тут Сбердевайсы?'\''"
+                    },
+                    "invoice_params": [
+                        {
+                            "key": "packageName",
+                            "value": "SbERdeVICEs"
+                        },
+                        {
+                            "key": "packageKey",
+                            "value": "ru.sberdevices.test"
+                        }
+                    ],
+                    "order": {
+                        "order_id": "a952b7ee-c928-4586-bd05-d932c21f749",
+                        "order_number": "1952",
+                        "order_date": "2021-04-07T08:24:37.729Z",
+                        "service_id": "13",
+                        "amount": 79900,
+                        "currency": "RUB",
+                        "purpose": "Покупка продуктов",
+                        "description": "Заказ № 22-1952. Покупка продуктов",
+                        "language": "ru-RU",
+                        "tax_system": 0,
+                        "order_bundle": [
+                            {
+                                "position_id": 1,
+                                "name": "Транзистор",
+                                "item_params": [
+                                    {
+                                        "key": "_itemAttributes_supplier_info.name",
+                                        "value": "ООО Ромашка"
+                                    },
+                                    {
+                                        "key": "_itemAttributes_supplier_info.inn",
+                                        "value": "5009053292"
+                                    },
+                                    {
+                                        "key": "_itemAttributes_nomenclature",
+                                        "value": "Å\u001e13622200005881"
+                                    },
+                                    {
+                                        "key": "_itemAttributes_agent_info.type",
+                                        "value": "7"
+                                    }
+
+                                ],
+                                "quantity": {
+                                    "value": 1,
+                                    "measure": "шт."
+                                },
+                                "item_amount": 79801,
+                                "currency": "RUB",
+                                "item_code": "21ff407a-fa98-11e8-80c5-0cc47a817926",
+                                "item_price": 79801,
+                                "discount_type": "amount",
+                                "discount_value": 17687,
+                                "tax_type": 6
+                            },
+                            {
+                                "position_id": 2,
+                                "name": "Тиристор",
+                                "item_params": [
+                                    {
+                                        "key": "code",
+                                        "value": "123значение321"
+                                    },
+                                    {
+                                        "key": "pack",
+                                        "value": "100"
+                                    },
+                                    {
+                                        "key": "_itemAttributes_supplier_info.name",
+                                        "value": "ООО Платежи"
+                                    },
+                                    {
+                                        "key": "_itemAttributes_supplier_info.inn",
+                                        "value": "7730253720"
+                                    }
+                                ],
+                                "quantity": {
+                                    "value": 1,
+                                    "measure": "шт."
+                                },
+                                "item_amount": 99,
+                                "currency": "RUB",
+                                "item_code": "21ff407c-fa98-11e8-80c5-0cc47a817926",
+                                "item_price": 99,
+                                "discount_type": "amount",
+                                "discount_value": 21,
+                                "tax_type": 6,
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    """
+
     def __init__(self, items, id=None):
         self.url = "/invoices"
         self.method = self.POST
@@ -43,6 +162,39 @@ class SmartPayCreateAction(SmartPayAction):
 
 
 class SmartPayPerformAction(SmartPayAction):
+
+    """
+    Example::
+        {
+            "type": "smartpay_perform",
+            "behavior": "some_behavior", // обязательный параметр
+            "invoice_id": "0", // обязательный параметр
+            "smartpay_params": { // обязательынй параметр
+                "user_id": {
+                    "partner_client_id": "2223"
+                },
+                "operations": [
+                    {
+                        "operation": "payment",
+                        "code": "invoice",
+                        "value": "167900"
+                    }
+                ],
+                "device_info": {
+                    "device_platform_type": "iOS",
+                    "device_platform_version": "13.6.1",
+                    "device_model": "iPhone 7",
+                    "device_manufacturer": "Apple",
+                    "device_id": "83c3f257-46d8-41fe-951b-f79d04e288c2",
+                    "surface": "SBOL",
+                    "surface_version": "11.5.0"
+                },
+                "return_url": "https://ok",
+                "fail_url": "https://fail"
+            }
+        }
+    """
+
     def __init__(self, items, id=None):
         self.url = f"/invoices/{items['invoice_id']}"
         self.method = self.POST
@@ -51,6 +203,18 @@ class SmartPayPerformAction(SmartPayAction):
 
 
 class SmartPayGetStatusAction(SmartPayAction):
+
+    """
+    Example::
+        {
+            "type": "smartpay_get_status",
+            "behavior": "some_behavior", // обязательный параметр
+            "invoice_id": "0", // обязательный параметр
+            "inv_status": "executed", // опциональный параметр
+            "wait": 50 // опциональный параметр
+        }
+    """
+
     def __init__(self, items, id=None):
         items["request_params"] = {}
         inv_status = items.get("inv_status")
@@ -69,6 +233,37 @@ class SmartPayGetStatusAction(SmartPayAction):
 
 
 class SmartPayConfirmAction(SmartPayAction):
+
+    """
+    Example::
+        {
+            "type": "smartpay_confirm",
+            "invoice_id": "0", // обязательный параметр
+            "smartpay_params": { // опциональный параметр, задаётся для неполной суммы (см. доку SmartPay API)
+                "invoice": {
+                    "order": {
+                        "amount": 79801,
+                        "currency": "RUB",
+                        "order_bundle": [
+                            {
+                                "position_id": 1,
+                                "name": "Транзистор",
+                                "quantity": {
+                                    "value": 1,
+                                    "measure": "шт."
+                                },
+                                "item_amount": 79801,
+                                "currency": "RUB",
+                                "item_code": "21ff407a-fa98-11e8-80c5-0cc47a817926",
+                                "item_price": 79801
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    """
+
     def __init__(self, items, id=None):
         self.url = f"/invoices/{items['invoice_id']}"
         self.method = self.PUT
@@ -77,6 +272,16 @@ class SmartPayConfirmAction(SmartPayAction):
 
 
 class SmartPayDeleteAction(SmartPayAction):
+
+    """
+    Example::
+        {
+            "type": "smartpay_delete",
+            "behavior": "my_behavior", // обязательынй параметр
+            "invoice_id": "0" // обязательынй параметр
+        }
+    """
+
     def __init__(self, items, id=None):
         self.url = f"/invoices/{items['invoice_id']}"
         self.method = self.DELETE
@@ -85,6 +290,36 @@ class SmartPayDeleteAction(SmartPayAction):
 
 
 class SmartPayRefundAction(SmartPayAction):
+
+    """
+    Example::
+        {
+            "type": "smartpay_refund",
+            "behavior": "some_behavior", // обязательный параметр
+            "invoice_id": "0", // обязательный параметр
+            "smartpay_params": { // опциональный параметр, задаётся при частичном возврате (см. доку SmartPay API)
+                "invoice": {
+                    "order": {
+                        "current_amount": 79900,
+                        "refund_amount": 79801,
+                        "currency": "RUB",
+                        "order_bundle": [
+                            {
+                                "position_id": 1,
+                                "name": "Транзистор",
+                                "quantity": {
+                                    "value": 1
+                                },
+                                "item_amount": 79801,
+                                "item_code": "21ff407a-fa98-11e8-80c5-0cc47a817926"
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    """
+
     def __init__(self, items, id=None):
         self.url = f"/invoices/{items['invoice_id']}"
         self.method = self.PATCH
